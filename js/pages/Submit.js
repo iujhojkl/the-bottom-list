0b823c98edac50a8e180f8633b9e466cfa66d0a8
lem <thientran65657@gmail.com>
2026-08-09 11:26:03 -0700

    Implement Submit component with embedded Google Form
    Added a default export for the Submit component with an embedded Google Form.

--- js/pages/Submit.js
+++ js/pages/Submit.js
@@ -1,1 +1,15 @@
-
+export default {
+    template: `
+        <main class="page-submit">
+            <div class="embed-container" style="display: flex; justify-content: center; padding: 20px;">
+                <iframe 
+                    src="https://docs.google.com/forms/d/e/1FAIpQLSdDLB3Mi1GgCxOC3bk0ZVJzHPB9K76z3dAsg7t3ytc_saFGTQ/viewform?embedded=true" 
+                    width="640" 
+                    height="800" 
+                    frameborder="0" 
+                    marginheight="0" 
+                    marginwidth="0">Loading…</iframe>
+            </div>
+        </main>
+    `,
+};
