59e7a14700548c53fae565bd71c818d37b74d97d
lem <thientran65657@gmail.com>
2026-08-09 11:32:22 -0700

    Refactor Submit.js for improved layout and styling
    Updated the layout and styling of the main container and iframe.

--- js/pages/Submit.js
+++ js/pages/Submit.js
@@ -1,15 +1,13 @@
 export default {
     template: `
-        <main class="page-submit">
-            <div class="embed-container" style="display: flex; justify-content: center; padding: 20px;">
-                <iframe 
-                    src="https://docs.google.com/forms/d/e/1FAIpQLSdDLB3Mi1GgCxOC3bk0ZVJzHPB9K76z3dAsg7t3ytc_saFGTQ/viewform?embedded=true" 
-                    width="640" 
-                    height="800" 
-                    frameborder="0" 
-                    marginheight="0" 
-                    marginwidth="0">Loading…</iframe>
-            </div>
+        <main class="page-submit" style="display: flex; flex-direction: column; align-items: center; justify-content: center; width: 100%; min-height: 80vh; padding: 20px 0;">
+            <iframe 
+                src="https://docs.google.com/forms/d/e/1FAIpQLSdDLB3Mi1GgCxOC3bk0ZVJzHPB9K76z3dAsg7t3ytc_saFGTQ/viewform?embedded=true" 
+                width="100%" 
+                style="max-width: 680px; border: none;"
+                height="800" 
+                frameborder="0" 
+                marginheight="0" 
+                marginwidth="0">Loading…</iframe>
         </main>
-    `,
 };
