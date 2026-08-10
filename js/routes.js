780843c088fd5d48e134fbb6168778d75f5a36b4
lem <thientran65657@gmail.com>
2026-08-09 18:53:36 -0700

    Add Packs route to routes.js

--- js/routes.js
+++ js/routes.js
@@ -2,10 +2,12 @@ import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 import Submit from './pages/Submit.js';
+import Packs from './pages/Packs.js';
 
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
     { path: '/submit', component: Submit },
+    { path: '/packs', component: Packs },
 ];
