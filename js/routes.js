864fe6745b97e52e4ff062fdc7df2f513ccaf769
lem <thientran65657@gmail.com>
2026-08-10 09:16:09 -0700

    Remove Packs route from routing configuration
    Removed Packs component from routes.

--- js/routes.js
+++ js/routes.js
@@ -2,12 +2,10 @@ import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 import Submit from './pages/Submit.js';
-import Packs from './pages/Packs.js';
 
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
     { path: '/submit', component: Submit },
-    { path: '/packs', component: Packs },
 ];
